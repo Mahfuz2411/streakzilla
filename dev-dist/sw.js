94f441a53181bd55a7ada5656538fe7fae4e7818
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2026-01-19 21:01:26 +0600

    feat: update service worker and HTML for PWA enhancements; add new apple touch icon and remove old SVG icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ujd4kcgfnkg"
+    "revision": "0.ujeis90qm78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
